0eceef38bbb4fe82898d8255f00d795866f656fb
matt <mattcave814@gmail.com>
2022-08-14 22:08:17 -0500

    compiled with webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         title: 'To-Do List',
-    //     }),
-    // ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-Do List',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        // clean: true,
+        clean: true,
     },
     module: {
         rules: [
